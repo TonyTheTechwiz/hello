e5d8ec3e6b8903aa5ce3cb18362d580d7f1e2c3b
Tony Calderon <tcalderon2@huskers.unl.edu>
2021-08-27 01:05:35 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tony Calderon
+ *         tcalderon2@husker.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tony Calderon\n");
+
+  return 0;
+}
